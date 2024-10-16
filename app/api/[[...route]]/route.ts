aff71d8fe00d8fe5f7d5c943d71ca5b3bfab9591
Тодор Христов <todorhristov2006@gmail.com>
2024-10-16 23:00:10 +0300

    Add protection to API

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,37 +2,27 @@ import {z} from "zod"
 import {zValidator} from "@hono/zod-validator"
 import { Hono } from "hono"
 import { handle } from "hono/vercel"
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 
 export const runtime = "edge"
 
 const app = new Hono().basePath("/api")
 
 app
-    .get("/hello", (c) => {
-        return c.json({
-            message: "Hello Next.js!",
-        })
-    })
     .get(
-        "/hello/:test", 
+        "/hello",
+        clerkMiddleware(),
         (c) => {
-        return c.json({
-            message: `Hello test!`,
-        })
-    })
-    .post(
-        "/create/:postId", 
-        zValidator("json", z.object({
-            name: z.string(),
-            userId: z.number(),
-        })),
-        zValidator("param", z.object({
-            postId: z.number(),
-        })),
-        (c) => {
-            const { name, userId} = c.req.valid("json");
-            const {postId} = c.req.valid("param");
-            return c.json({});
-        })
+            const auth = getAuth(c);
+
+            if (!auth?.userId) {
+                return c.json({error: "Unauthorized"});
+            }
+
+            return c.json({
+                message: "Hello Next.js!",
+                userId: auth.userId,
+            })
+    });
 export const GET = handle(app)
 export const POST = handle(app)
