6da44203724b3866cd4885d30b91053f25484ad6
Тодор Христов <todorhristov2006@gmail.com>
2024-10-16 23:16:09 +0300

    Add temp files

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -4,25 +4,30 @@ import { Hono } from "hono"
 import { handle } from "hono/vercel"
 import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 
+import authors from './authors'
+import books from './books'
+
 export const runtime = "edge"
 
-const app = new Hono().basePath("/api")
+const app = new Hono().basePath("/api");
 
-app
-    .get(
-        "/hello",
-        clerkMiddleware(),
-        (c) => {
-            const auth = getAuth(c);
+const routes = app.route("/authors", authors).route("/books", books);
+// app
+//     .get(
+//         "/hello",
+//         clerkMiddleware(),
+//         (c) => {
+//             const auth = getAuth(c);
 
-            if (!auth?.userId) {
-                return c.json({error: "Unauthorized"});
-            }
+//             if (!auth?.userId) {
+//                 return c.json({error: "Unauthorized"});
+//             }
 
-            return c.json({
-                message: "Hello Next.js!",
-                userId: auth.userId,
-            })
-    });
+//             return c.json({
+//                 message: "Hello Next.js!",
+//                 userId: auth.userId,
+//             })
+//     });
 export const GET = handle(app)
 export const POST = handle(app)
+export type AppType = typeof routes
