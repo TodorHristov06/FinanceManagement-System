4bb6c1cfd9860149095d356614b502a01828cf4a
Тодор Христов <todorhristov2006@gmail.com>
2024-10-20 23:49:57 +0300

    Fix mistake

--- components/ui/welcome-msg.tsx
+++ components/ui/welcome-msg.tsx
@@ -8,7 +8,7 @@ export const WelcomeMSG = () => {
     return(
         <div className="space-y-2 mb-4">
             <h2 className="text-2xl lg:text-4xl text-white font-medium">
-                Welcome Back{isLoaded ? ", " : ""}{user?.username} //user?.firstName
+                Welcome Back{isLoaded ? ", " : ""}{user?.username}
             </h2>
             <p className="text-sm lg:text-base text-[#89b6fd]">
                 This is your Financial Overview Report
