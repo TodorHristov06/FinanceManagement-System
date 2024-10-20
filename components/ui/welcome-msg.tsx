1737aab101661f41672392ef7814b9255cd0132d
Тодор Христов <todorhristov2006@gmail.com>
2024-10-20 23:23:13 +0300

    Update msg style

--- components/ui/welcome-msg.tsx
+++ components/ui/welcome-msg.tsx
@@ -6,11 +6,11 @@ export const WelcomeMSG = () => {
     const {user, isLoaded} = useUser();
 
     return(
-        <div>
-            <h2>
+        <div className="space-y-2 mb-4">
+            <h2 className="text-2xl lg:text-4xl text-white font-medium">
                 Welcome Back
             </h2>
-            <p>
+            <p className="text-sm lg:text-base text-[#89b6fd]">
                 This is your Financial Overview Report
             </p>
         </div>
