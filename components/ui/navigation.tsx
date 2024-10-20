a12d43c0fa4bc46853a788a03cd8a1a0831f461b
Тодор Христов <todorhristov2006@gmail.com>
2024-10-20 17:25:48 +0300

    Add nav labels to navigation

--- components/ui/navigation.tsx
+++ components/ui/navigation.tsx
@@ -1,9 +1,34 @@
 "use client";
 
+const routes = [
+    {
+        href: "/",
+        label: "Overview",
+    },
+    {
+        href: "/transactions",
+        label: "Transactions",
+    },
+    {
+        href: "/accounts",
+        label: "Accounts",
+    },
+    {
+        href: "/categories",
+        label: "Categories",
+    },
+    {
+        href: "/settings",
+        label: "Settings",
+    },
+];
+
 export const Navigation = () => {
     return (
-        <div>
-            Navigation
-        </div>
+        <nav className="hidden lg:flex items-center gap-x-2 overflow-x-auto">
+           {routes.map((route) => (
+               <p>{route.label}</p>
+           ))}
+        </nav>
     )
 }
