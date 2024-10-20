e41d660bdecee363724a97d17963e8cc7761bf56
Тодор Христов <todorhristov2006@gmail.com>
2024-10-20 14:46:43 +0300

    Import header-logo in header component

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,3 +1,5 @@
+import { HeaderLogo } from "./header-logo";
+
 export const Header = () => {
     return (
         <header className="bg-gradient-to-b from-blue-700 to-blue-500 px-4 py-8 lg:px-14 pb-36">
