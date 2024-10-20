5d1b4aab16b3dfd9f63794006afd0b5d47bdd836
Тодор Христов <todorhristov2006@gmail.com>
2024-10-20 14:53:16 +0300

    Set a name and logo to the header-logo component

--- components/ui/header-logo.tsx
+++ components/ui/header-logo.tsx
@@ -4,8 +4,11 @@ import Image from "next/image";
 export const HeaderLogo = () => {
     return (
         <Link href="/">
-            <div>
-                Logo
+            <div className="items-center hidden lg:flex">
+                <Image src="/logo.svg" height={28} width={28} alt="logo"/>
+                <p className="font-semibold text-white text-2xl ml-2.5">
+                    AtlasVault 
+                </p>
             </div>
         </Link>
     )
