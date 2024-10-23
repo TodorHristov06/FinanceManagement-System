fe5ff15a1678bb456f3fe05661f073ee94fe98f8
Тодор Христов <todorhristov2006@gmail.com>
2024-10-23 18:42:45 +0300

    Update route for accounts

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,13 +1,13 @@
 import { Hono } from "hono"
 import { handle } from "hono/vercel"
 
+import accounts from "./accounts";
+
 export const runtime = "edge"
 
 const app = new Hono().basePath("/api");
 
-app.get("/hello", (c) => {
-    return c.json({ hello: "world" });
-});
+app.route("/accounts", accounts);
 
-export const GET = handle(app)
-export const POST = handle(app)
+export const GET = handle(app);
+export const POST = handle(app);
