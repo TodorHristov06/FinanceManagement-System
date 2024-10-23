85c45ba233fbe74eaeea526aa357e7cc92f3010f
Тодор Христов <todorhristov2006@gmail.com>
2024-10-23 18:49:25 +0300

    Async controller

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -7,7 +7,9 @@ export const runtime = "edge"
 
 const app = new Hono().basePath("/api");
 
-app.route("/accounts", accounts);
+const routes = app.route("/accounts", accounts);
 
 export const GET = handle(app);
 export const POST = handle(app);
+
+export type AppType = typeof routes;
