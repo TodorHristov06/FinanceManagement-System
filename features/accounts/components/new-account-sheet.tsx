a1be9c438d3b735958e11f8cfdd46338df1c819c
Тодор Христов <todorhristov2006@gmail.com>
2024-10-27 18:41:46 +0200

    Update closed-open sheet

--- features/accounts/components/new-account-sheet.tsx
+++ features/accounts/components/new-account-sheet.tsx
@@ -1,8 +1,11 @@
+import { useNewAccounts } from "@/features/accounts/hooks/use-new-accounts";
 import {Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle} from "@/components/ui/sheet"
 
+
 export const NewAccountSheet = () => {
+    const { isOpen, onClose } = useNewAccounts();
     return (
-        <Sheet open>
+        <Sheet open={isOpen} onOpenChange={onClose}>
             <SheetContent className="space-y-4">
                 <SheetHeader>
                     <SheetTitle>
