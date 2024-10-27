5ee8a047b9fe98dddf4a3a02ad60d128b1f160b0
Тодор Христов <todorhristov2006@gmail.com>
2024-10-27 21:31:49 +0200

    Update account-sheet

--- features/accounts/components/new-account-sheet.tsx
+++ features/accounts/components/new-account-sheet.tsx
@@ -1,11 +1,23 @@
 import { useNewAccounts } from "@/features/accounts/hooks/use-new-accounts";
 import { AccountForm } from "@/features/accounts/components/account-form";
 import {Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle} from "@/components/ui/sheet";
+import { FormValue } from "hono/types";
+import { insertAccountSchema } from "@/db/schema";
+import { z } from "zod";
 
+const formSchema = insertAccountSchema.pick({
+    name: true,
+})
+
+type FormValues = z.input<typeof formSchema>
 
 
 export const NewAccountSheet = () => {
     const { isOpen, onClose } = useNewAccounts();
+    
+    const onSubmit = (values: FormValues) => {
+        console.log({ values });
+    };
     return (
         <Sheet open={isOpen} onOpenChange={onClose}>
             <SheetContent className="space-y-4">
@@ -17,7 +29,7 @@ export const NewAccountSheet = () => {
                         Create a new account to track your transactions.
                     </SheetDescription>
                 </SheetHeader>
-                <AccountForm onSubmit={() => {}} disabled={false}/>
+                <AccountForm onSubmit={onSubmit} disabled={false}/>
             </SheetContent>
         </Sheet> 
     );
