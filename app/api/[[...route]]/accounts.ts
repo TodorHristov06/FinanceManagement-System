0cc8809b0193501c88554c87d7a8de8595608304
Тодор Христов <todorhristov2006@gmail.com>
2024-10-27 16:30:06 +0200

    Update accounts

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,8 +1,9 @@
 import { Hono } from "hono"
 import { db } from "@/db/drizzle"
-import { accounts } from "@/db/schema";
+import { accounts, insertAccountSchema } from "@/db/schema";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { eq } from "drizzle-orm";
+import {zValidator} from "@hono/zod-validator";
 
 const app = new Hono()
     .get("/",
@@ -23,5 +24,26 @@ const app = new Hono()
             .where(eq(accounts.userId, auth.userId));
             return c.json({ data });
     })
+    .post("/",
+        clerkMiddleware(),
+        zValidator("json", insertAccountSchema.pick({
+            name: true,
+
+        })),
+        async(c) => {
+            const auth = getAuth(c);
+            const values = c.req.valid("json")
+
+            if(!auth?.userId){
+                return c.json({ error: "Unauthorized" }, 401);
+            }
+
+            const data = await db.insert(accounts).values({
+                id: "test",
+                userId: auth.userId,
+                ...values,
+            })
+            return c.json({})
+        })
 
 export default app;
