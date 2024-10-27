84f2086975606eedc2d8951142571759cb5897c1
Тодор Христов <todorhristov2006@gmail.com>
2024-10-27 17:56:40 +0200

    Update sheet-provider

--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -1,8 +1,14 @@
 "use client";
 
-import { NewAccountSheet } from "@/components/new-account-sheet";
+import { useMountedState } from "react-use";
+import { useEffect, useState } from "react";
+import { NewAccountSheet } from "@/features/accounts/components/new-account-sheet";
 
 export const SheetProvider = () => {
+    const isMounted = useMountedState();
+
+    if (!isMounted) return null;
+
     return (
         <>
             <NewAccountSheet/>
