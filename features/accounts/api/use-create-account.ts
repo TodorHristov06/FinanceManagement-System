9ba27543463e711beb9c75d6db5a123cf6bb6d53
Тодор Христов <todorhristov2006@gmail.com>
2024-10-28 00:08:31 +0200

    Fix typo

--- features/accounts/api/use-create-account.ts
+++ features/accounts/api/use-create-account.ts
@@ -2,20 +2,20 @@ import { InferRequestType, InferResponseType } from "hono";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "sonner";
 import { client} from "@/lib/hono";
-type ResponceType = InferResponseType<typeof client.api.accounts.$post>;
+type ResponseType = InferResponseType<typeof client.api.accounts.$post>;
 type RequestType = InferRequestType<typeof client.api.accounts.$post>["json"];
 
 export const useCreateAccount = () => {
     const queryClient = useQueryClient();
 
     const mutation = useMutation<
-        ResponceType,
+        ResponseType,
         Error,
         RequestType
     >({
         mutationFn: async (json) => {
-            const responce = await client.api.accounts.$post({ json });
-            return await responce.json();
+            const response = await client.api.accounts.$post({ json });
+            return await response.json();
         },
         onSuccess: () => {
             toast.success("Account created");
