114d0a6bf2992226d033de38f5e8b57be4b01e7b
Тодор Христов <todorhristov2006@gmail.com>
2024-10-27 18:48:49 +0200

    Add button on click to page.tsx (dashboard)

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,35 +1,15 @@
-import { UserButton } from "@clerk/nextjs";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useNewAccounts } from "@/features/accounts/hooks/use-new-accounts";
 
 export default function Home() {
+  const { onOpen } = useNewAccounts();
   return (
       <div>
-        Dashboard Page
+        <Button onClick={onOpen}>
+            Add an account
+        </Button>
       </div>
   );
-}
-
-// "use client";
-
-// import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
-
-// export default function Home() {
-//   const { data: accounts, isLoading} = useGetAccounts();
-
-//   if (isLoading){
-//     return (
-//       <div>
-//         Loading...
-//       </div>
-//     )
-//   }
-
-//   return (
-//       <div>
-//         {accounts?.map((account) => (
-//           <div key={account.id}>
-//             {account.name}
-//           </div>
-//         ))}
-//       </div>
-//   );
-// }
+};
