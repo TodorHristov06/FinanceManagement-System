464761a6515949fe35c5ac7db98b5ba3b4399cb4
Тодор Христов <todorhristov2006@gmail.com>
2024-10-31 17:14:14 +0200

    Change card design

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -7,9 +7,9 @@ import { Plus } from "lucide-react";
 
 const AccountsPage = () => {
     const newAccounts = useNewAccounts();
-    
+
     return (
-        <div className="max-w-screen-2xl mx-auto">
+        <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
             <Card className="border-none drop-shadow-sm">
                 <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
                     <CardTitle className="text-xl line-clamp-1">
