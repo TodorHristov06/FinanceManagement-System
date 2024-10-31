2b58c6f6e35524cdb379fac6f4b44b216b40a894
Тодор Христов <todorhristov2006@gmail.com>
2024-10-31 16:40:50 +0200

    Add button for accounts

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -1,13 +1,20 @@
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Plus } from "lucide-react";
 
 const AccountsPage = () => {
     return (
         <div>
-            <Card>
-                <CardHeader>
-                    <CardTitle>
+            <Card className="border-none drop-shadow-sm">
+                <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+                    <CardTitle className="text-xl line-clamp-1">
                         Accounts Page  
                     </CardTitle>
+                    <Button>
+                        <Plus>
+                            Add new
+                        </Plus>
+                    </Button>
                 </CardHeader>
             </Card>
         </div>
