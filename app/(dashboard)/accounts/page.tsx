978351dc17241cb4fb3d83f056a01b1cc3da303b
Тодор Христов <todorhristov2006@gmail.com>
2024-10-31 17:48:37 +0200

    Update page.tsx

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -4,6 +4,17 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useNewAccounts } from "@/features/accounts/hooks/use-new-accounts";
 import { Plus } from "lucide-react";
+import { columns, Payment } from "./columns";
+import { DataTable } from "@/components/data-table";
+
+const data: Payment[] = [
+{
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+},
+]
 
 const AccountsPage = () => {
     const newAccounts = useNewAccounts();
@@ -20,6 +31,9 @@ const AccountsPage = () => {
                         Add new
                     </Button>
                 </CardHeader>
+                <CardContent>
+                    <DataTable columns={columns} data={data} />
+                </CardContent>
             </Card>
         </div>
     )
