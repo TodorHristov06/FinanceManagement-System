52a9ff8b7ede6b8bf8649334bee870063fbc9c2a
Тодор Христов <todorhristov2006@gmail.com>
2024-10-31 18:31:44 +0200

    Add sorting button for "Email"

--- app/(dashboard)/accounts/columns.tsx
+++ app/(dashboard)/accounts/columns.tsx
@@ -2,7 +2,7 @@
 
 import { Button } from "@/components/ui/button"
 import { ColumnDef } from "@tanstack/react-table"
-import { ArrowUpDown, MoreHorizontal } from "lucide-react"
+import { ArrowUpDown } from "lucide-react"
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -22,10 +22,7 @@ export const columns: ColumnDef<Payment>[] = [
     accessorKey: "email",
     header: ({ column }) => {
       return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
+        <Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
           Email
           <ArrowUpDown className="ml-2 h-4 w-4" />
         </Button>
--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -8,12 +8,18 @@ import { columns, Payment } from "./columns";
 import { DataTable } from "@/components/data-table";
 
 const data: Payment[] = [
-{
-    id: "728ed52f",
-    amount: 100,
-    status: "pending",
-    email: "m@example.com",
-},
+    {
+        id: "728ed52f",
+        amount: 100,
+        status: "pending",
+        email: "m@example.com",
+    },
+    {
+        id: "728ed52f",
+        amount: 50,
+        status: "success",
+        email: "a@example.com",
+    },
 ]
 
 const AccountsPage = () => {
