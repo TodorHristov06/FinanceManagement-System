dbc55d134931c10f088b222fea13026c7409ecf7
Тодор Христов <todorhristov2006@gmail.com>
2024-11-03 20:38:21 +0200

    Import dialog to use-confirm hook

---
+++ hooks/use-confirm.tsx
@@ -0,0 +1,4 @@
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogDescription ,DialogHeader, DialogTitle, DialogFooter } from "@/components/ui/dialog";
