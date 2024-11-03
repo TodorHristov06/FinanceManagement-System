a4a11dbcc5c670dc23992e2bdf91c06aaf098ea2
Тодор Христов <todorhristov2006@gmail.com>
2024-11-03 17:39:55 +0200

    Add animation for spin

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -3,16 +3,34 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useNewAccounts } from "@/features/accounts/hooks/use-new-accounts";
-import { Plus } from "lucide-react";
+import { Loader2, Plus } from "lucide-react";
 import { columns } from "./columns";
 import { DataTable } from "@/components/data-table";
 import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+import { Skeleton } from "@/components/ui/skeleton";
 
 
 const AccountsPage = () => {
     const newAccounts = useNewAccounts();
     const accountsQuery = useGetAccounts();
     const accounts = accountsQuery.data || [];
+
+    if (accountsQuery.isLoading) {
+        return(
+            <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+                <Card className="border-none drop-shadow-sm">
+                    <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+                        <Skeleton className="h-8 w-48"/>
+                    </CardHeader>
+                    <CardContent>
+                        <div className="h-[500px] w-full flex justify-center items-center">
+                            <Loader2 className="size-6 text-slate-300 animate-spin" />
+                        </div>
+                    </CardContent>
+                </Card>
+            </div>
+        )
+    }
 
     return (
         <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
