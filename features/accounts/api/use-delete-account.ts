640cceaf0ca7de4d323dde21d64885736a93fa38
Тодор Христов <todorhristov2006@gmail.com>
2024-11-09 17:51:03 +0200

    Update use-delete-accounts.ts

--- features/accounts/api/use-delete-account.ts
+++ features/accounts/api/use-delete-account.ts
@@ -3,32 +3,29 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "sonner";
 import { client} from "@/lib/hono";
 
-type ResponseType = InferResponseType<typeof client.api.accounts[":id"]["$patch"]>;
-type RequestType = InferRequestType<typeof client.api.accounts[":id"]["$patch"]>["json"];
+type ResponseType = InferResponseType<typeof client.api.accounts[":id"]["$delete"]>;
 
-export const useEditAccount = (id?: string) => {
+export const useDeleteAccount = (id?: string) => {
     const queryClient = useQueryClient();
 
     const mutation = useMutation<
         ResponseType,
-        Error,
-        RequestType
+        Error
     >({
-        mutationFn: async (json) => {
-            const response = await client.api.accounts[":id"]["$patch"]({ 
-                param: { id }, 
-                json 
+        mutationFn: async () => {
+            const response = await client.api.accounts[":id"]["$delete"]({ 
+                param: { id }
             });
             return await response.json();
         },
         onSuccess: () => {
-            toast.success("Account updated");
+            toast.success("Account deleted");
             queryClient.invalidateQueries({ queryKey: ["account", { id }] });
             queryClient.invalidateQueries({ queryKey: ["accounts"] });
             //TODO: invalidate summary and transactions
         },
         onError: () => {
-            toast.error("Failed to edit account");
+            toast.error("Failed to delete account");
         },
     })
 
