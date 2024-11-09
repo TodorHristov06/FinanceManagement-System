65d16cff9ed808922bc422802d08cb7ff02d857a
Тодор Христов <todorhristov2006@gmail.com>
2024-11-09 17:01:25 +0200

    Update edit-account-sheet

--- features/accounts/components/edit-account-sheet.tsx
+++ features/accounts/components/edit-account-sheet.tsx
@@ -5,6 +5,7 @@ import { useOpenAccount } from "@/features/accounts/hooks/use-open-account";
 import { useGetAccount } from "@/features/accounts/api/use-get-account";
 import {Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle} from "@/components/ui/sheet";
 import { insertAccountSchema } from "@/db/schema";
+import { Loader2 } from "lucide-react";
 
 
 
@@ -19,8 +20,9 @@ export const EditAccountSheet = () => {
     const { isOpen, onClose, id } = useOpenAccount();
 
     const accountQuery = useGetAccount(id)
-
     const mutation = useCreateAccount();
+
+    const isLoading = accountQuery.isLoading
     
     const onSubmit = (values: FormValues) => {
         mutation.mutate(values, {
@@ -47,7 +49,17 @@ export const EditAccountSheet = () => {
                         Create a new account to track your transactions.    
                     </SheetDescription>
                 </SheetHeader>
-                <AccountForm onSubmit={onSubmit} disabled={mutation.isPending} defaultValues={defaultValues}/>
+                {isLoading 
+                    ?(
+                        <div className="absolute inset-0 flex items-center justify-center" >
+                            <Loader2 className="size-4 text-muted-foreground animate-spin" />
+                        </div> 
+                    ) : (
+                        <AccountForm onSubmit={onSubmit} disabled={mutation.isPending} defaultValues={defaultValues}/>
+                    )
+                }
+                    
+                
             </SheetContent>
         </Sheet> 
     );
