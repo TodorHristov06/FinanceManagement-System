da4c1e364671c2228d53965f07649a7acf2668da
TodorHristov06 <todorhristov2006@gmail.com>
2024-11-19 23:55:04 +0200

    Add comment to providers

--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -1,14 +1,17 @@
 "use client";
-
+// Importing a custom hook to check if the component is mounted
 import { useMountedState } from "react-use";
+// Importing the NewAccountSheet component
 import { NewAccountSheet } from "@/features/accounts/components/new-account-sheet";
+// Importing the EditAccountSheet component
 import { EditAccountSheet } from "@/features/accounts/components/edit-account-sheet";
-
+// SheetProvider component, acting as a central place to manage sheet components
 export const SheetProvider = () => {
+    // Using the useMountedState hook to ensure the component is only rendered on the client
     const isMounted = useMountedState();
-
+    // If the component is not mounted yet, return null to avoid rendering
     if (!isMounted) return null;
-
+    // Render the NewAccountSheet and EditAccountSheet components
     return (
         <>
             <NewAccountSheet/>
