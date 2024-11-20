d83f271023559029f95e1d3d340868bdcbe3309e
TodorHristov06 <todorhristov2006@gmail.com>
2024-11-20 19:09:21 +0200

    Add categories in dashboard from accounts

---
+++ app/(dashboard)/categories/columns.tsx
@@ -0,0 +1,53 @@
+"use client"; // Mark this component to be rendered on the client side
+
+import { Button } from "@/components/ui/button"; // Import Button component from UI library
+import { ColumnDef } from "@tanstack/react-table"; // Import ColumnDef for defining table columns
+import { ArrowUpDown } from "lucide-react"; // Import sorting icon
+import { Checkbox } from "@/components/ui/checkbox"; // Import Checkbox component from UI library       
+import { InferResponseType } from "hono"; // Type inference for the API response
+import { client } from "@/lib/hono"; // Import client for API communication
+import { Actions } from "./actions"; // Import Actions component for each row
+
+// Define response type from API to infer account data shape
+export type ResponseType = InferResponseType<typeof client.api.accounts.$get, 200>["data"] [0]
+
+// Define table columns
+export const columns: ColumnDef<ResponseType>[] = [
+  {
+    id: "select", // Define the 'select' column for selecting rows
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false, // Disable sorting for this column
+    enableHiding: false, // Disable hiding for this column
+  },
+  {
+    accessorKey: "name", // Define column for account name
+    header: ({ column }) => {
+      return (
+        <Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+          Name
+          <ArrowUpDown className="ml-2 h-4 w-4" /> {/* Sorting icon */}
+        </Button>
+      )
+    },
+  },
+  {
+    id: "actions", // Define the 'actions' column for each row
+    cell: ({ row }) => <Actions id={row.original.id} /> // Render the Actions component with the account ID
+  }
+]
---
+++ app/(dashboard)/categories/page.tsx
@@ -0,0 +1,70 @@
+"use client"; // Mark this component to be rendered on the client side
+
+import { Button } from "@/components/ui/button"; // Import Button component from UI library
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"; // Import Card components for layout
+import { Loader2, Plus } from "lucide-react"; // Import icons for loading and adding
+import { columns } from "./columns"; // Import column definitions for the table
+import { DataTable } from "@/components/data-table"; // Import DataTable component for displaying accounts
+import { Skeleton } from "@/components/ui/skeleton"; // Import Skeleton component for loading state
+import { useBulkDeleteAccounts } from "@/features/accounts/api/use-bulk-delete"; // Import bulk delete hook
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts"; // Import hook for fetching accounts
+import { useNewAccounts } from "@/features/accounts/hooks/use-new-accounts"; // Import hook for creating new accounts
+
+
+const CategoriesPage = () => {
+    const newAccounts = useNewAccounts(); 
+    const deleteAccount = useBulkDeleteAccounts(); 
+    const accountsQuery = useGetAccounts(); 
+    const accounts = accountsQuery.data || []; 
+
+    const isDisabled =
+        accountsQuery.isLoading ||
+        deleteAccount.isPending; 
+
+    if (accountsQuery.isLoading) {
+        return(
+            <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+                <Card className="border-none drop-shadow-sm">
+                    <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+                        <Skeleton className="h-8 w-48"/>
+                    </CardHeader>
+                    <CardContent>
+                        <div className="h-[500px] w-full flex justify-center items-center">
+                            <Loader2 className="size-6 text-slate-300 animate-spin" />
+                        </div>
+                    </CardContent>
+                </Card>
+            </div>
+        )
+    }
+
+    return (
+        <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+            <Card className="border-none drop-shadow-sm">
+                <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+                    <CardTitle className="text-xl line-clamp-1">
+                        Categories Page  
+                    </CardTitle>
+                    <Button onClick={newAccounts.onOpen} size="sm">
+                        <Plus className="size-4 mr-2"/>
+                        Add new
+                    </Button>
+                </CardHeader>
+                <CardContent>
+                    <DataTable 
+                        filterKey="email" 
+                        columns={columns} 
+                        data={accounts} 
+                        onDelete={(row: any[]) => {
+                            const ids = row.map((r: any) => r.original.id); 
+                            deleteAccount.mutate({ids});
+                        }}
+                        disabled={isDisabled}
+                    />
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
+
+export default CategoriesPage;
