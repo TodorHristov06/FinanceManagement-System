2e6138eff5bf48ec55626a7a198e768efde23674
TodorHristov06 <todorhristov2006@gmail.com>
2024-11-20 20:17:29 +0200

    Import categories in sheet-provider.tsx

--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -1,21 +1,25 @@
 "use client";
-// Importing a custom hook to check if the component is mounted
-import { useMountedState } from "react-use";
-// Importing the NewAccountSheet component
-import { NewAccountSheet } from "@/features/accounts/components/new-account-sheet";
-// Importing the EditAccountSheet component
-import { EditAccountSheet } from "@/features/accounts/components/edit-account-sheet";
+
+import { useMountedState } from "react-use"; // Importing a custom hook to check if the component is mounted
+import { NewAccountSheet } from "@/features/accounts/components/new-account-sheet"; // Importing the NewAccountSheet component
+import { EditAccountSheet } from "@/features/accounts/components/edit-account-sheet"; // Importing the EditAccountSheet component
+
+import { NewCategorySheet } from "@/features/categories/components/new-category-sheet"; // Importing the NewCategorySheet component
+import { EditCategorySheet } from "@/features/categories/components/edit-category-sheet"; // Importing the EditCategorySheet component
 // SheetProvider component, acting as a central place to manage sheet components
 export const SheetProvider = () => {
     // Using the useMountedState hook to ensure the component is only rendered on the client
     const isMounted = useMountedState();
     // If the component is not mounted yet, return null to avoid rendering
     if (!isMounted) return null;
-    // Render the NewAccountSheet and EditAccountSheet components
+    // Render the NewAccountSheet and EditAccountSheet / NewCategorySheet and EditCategorySheet components
     return (
         <>
             <NewAccountSheet/>
             <EditAccountSheet/>
+
+            <NewCategorySheet/>
+            <EditCategorySheet/>
         </>
     )
 }
