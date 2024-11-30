7d17c4caf2389dc6dbce7d328ae89f40901571c4
TodorHristov06 <todorhristov2006@gmail.com>
2024-11-30 19:45:24 +0200

    Update queries for transactions

--- app/api/[[...route]]/transactions.ts
+++ app/api/[[...route]]/transactions.ts
@@ -2,30 +2,65 @@
 import { Hono } from "hono";
 import { db } from "@/db/drizzle";
 import { createId} from "@paralleldrive/cuid2";
-import { categories, insertCategorySchema } from "@/db/schema";
+import { transactions, insertTransactionSchema, categories, accounts } from "@/db/schema";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
-import { eq, and, inArray } from "drizzle-orm";
+import { eq, and, inArray, gte, lte, desc } from "drizzle-orm";
 import {zValidator} from "@hono/zod-validator";
 import { z } from "zod";
+import { parse, subDays } from "date-fns";
+import { date } from "drizzle-orm/mysql-core";
+
 
 const app = new Hono()
 
     .get(
         "/",
+        zValidator("query", z.object({
+            from: z.string().optional(),
+            to: z.string().optional(),
+            accountId: z.string().optional(),
+        })),
         clerkMiddleware(), 
          async (c) => {
             const auth = getAuth(c);
+            const { from, to, accountId } = c.req.valid("query")
 
             if(!auth?.userId){
                 return c.json({ error: "Unauthorized" }, 401);
             }
+
+            const defaultTo = new Date();
+            const defaultFrom = subDays(defaultTo, 30);
+
+            const startDate = from ? parse(from, "yyyy-MM-dd", new Date()) : defaultFrom;
+            const endDate = to ? parse(to, "yyyy-MM-dd", new Date()) : defaultTo;
+
             const data = await db
                 .select({
-                    id: categories.id,
-                    name: categories.name,
+                    id: transactions.id,
+                    date: transactions.date,
+                    category: categories.name,
+                    categoryId: transactions.categoryId,
+                    payee: transactions.payee,
+                    amount: transactions.amount,
+                    notes: transactions.notes,
+                    account: accounts.name,
+                    accountId: transactions.accountId,
+
                 })
-            .from(categories)
-            .where(eq(categories.userId, auth.userId));
+                .from(transactions)
+                .innerJoin(accounts, eq(transactions.accountId, accounts.id))
+                .leftJoin(categories, eq(transactions.categoryId, categories.id))
+                .where(
+                    and(
+                        accountId ? eq(transactions.accountId, accountId) : undefined,
+                        eq(accounts.userId, auth.userId),
+                        gte(transactions.date, startDate),
+                        lte(transactions.date, endDate),
+                    )
+                )
+                .orderBy(desc(transactions.date));
+
             return c.json({ data });
     })
     .get(
@@ -47,11 +82,20 @@ const app = new Hono()
             }
             const [data] = await db
                 .select({
-                    id: categories.id,
-                    name: categories.name,
+                    id: transactions.id,
+                    date: transactions.date,
+                    categoryId: transactions.categoryId,
+                    payee: transactions.payee,
+                    amount: transactions.amount,
+                    notes: transactions.notes,
+                    accountId: transactions.accountId,
                 })
-            .from(categories)
-            .where(and(eq(categories.userId, auth.userId), eq(categories.id, id)));
+                .from(transactions)
+                .innerJoin(accounts, eq(transactions.accountId, accounts.id))
+                .where(and(
+                    eq(transactions.id, id), 
+                    eq(accounts.userId, auth.userId),
+                ));
 
             if (!data){
                 return c.json({ error: "Category not found" }, 404);
@@ -62,8 +106,8 @@ const app = new Hono()
     .post(
         "/",
         clerkMiddleware(),
-        zValidator("json", insertCategorySchema.pick({
-            name: true,
+        zValidator("json", insertTransactionSchema.omit({
+            id: true,
         })),
         async(c) => {
             const auth = getAuth(c);
@@ -72,11 +116,11 @@ const app = new Hono()
             if(!auth?.userId){
                 return c.json({ error: "Unauthorized" }, 401);
             }
-            const [data] = await db.insert(categories).values({
+            const [data] = await db.insert(transactions).values({
                 id: createId(),
-                userId: auth.userId,
                 ...values,
             }).returning(); 
+            
             return c.json({ data })
     })
     .post(
