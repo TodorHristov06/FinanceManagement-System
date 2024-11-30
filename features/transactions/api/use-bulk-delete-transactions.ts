d9833bed49672d71e4f8e495885ee6c6421c11cc
TodorHristov06 <todorhristov2006@gmail.com>
2024-11-30 20:59:33 +0200

    Update use-bulk-delete-transactions.ts queryKey and requests

--- features/transactions/api/use-bulk-delete-transactions.ts
+++ features/transactions/api/use-bulk-delete-transactions.ts
@@ -4,11 +4,11 @@ import { toast } from "sonner"; // Importing toast notifications
 import { client } from "@/lib/hono"; // Importing the Hono API client
 
 // Inferring the types for request and response from the Hono API endpoint
-type ResponseType = InferResponseType<typeof client.api.accounts["bulk-delete"]["$post"]>;
-type RequestType = InferRequestType<typeof client.api.accounts["bulk-delete"]["$post"]>["json"];
+type ResponseType = InferResponseType<typeof client.api.transactions["bulk-delete"]["$post"]>;
+type RequestType = InferRequestType<typeof client.api.transactions["bulk-delete"]["$post"]>["json"];
 
-// Custom hook for handling bulk deletion of accounts
-export const useBulkDeleteAccounts = () => {
+// Custom hook for handling bulk deletion of transactions
+export const useBulkDeleteTransactions = () => {
     const queryClient = useQueryClient(); // React Query client for cache management
     // Using the mutation hook to handle the deletion
     const mutation = useMutation<
@@ -17,16 +17,16 @@ export const useBulkDeleteAccounts = () => {
         RequestType
     >({
         mutationFn: async (json) => {
-            const response = await client.api.accounts["bulk-delete"]["$post"]({ json }); // Sending the API request
+            const response = await client.api.transactions["bulk-delete"]["$post"]({ json }); // Sending the API request
             return await response.json(); // Parsing the response JSON
         }, 
         onSuccess: () => {
-            toast.success("Accounts deleted"); // Show success toast on deletion
-            queryClient.invalidateQueries({ queryKey: ["accounts"] }); // Invalidate the accounts query to refetch data
+            toast.success("Transactions deleted"); // Show success toast on deletion
+            queryClient.invalidateQueries({ queryKey: ["transactions"] }); // Invalidate the transactions query to refetch data
             //TODO: invalidate summary
         },
         onError: () => {
-            toast.error("Failed to delete accounts"); // Show error toast on failure
+            toast.error("Failed to delete transactions"); // Show error toast on failure
         },
     })
 
