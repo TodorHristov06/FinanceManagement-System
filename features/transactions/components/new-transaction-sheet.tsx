4e265cd788fb962318aa7636248d054fb21ec0ba
TodorHristov06 <todorhristov2006@gmail.com>
2024-12-03 00:00:57 +0200

    Update new=transaction-sheet to use category,account and transaction

--- features/transactions/components/new-transaction-sheet.tsx
+++ features/transactions/components/new-transaction-sheet.tsx
@@ -1,9 +1,13 @@
 // Importing necessary libraries and components
 import { z } from "zod";  // For schema validation using Zod
-import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from "@/components/ui/sheet";  // UI components for sheet layout
 import { insertTransactionSchema } from "@/db/schema";
+import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from "@/components/ui/sheet";  // UI components for sheet layout
 import { useNewTransaction } from "@/features/transactions/hooks/use-new-transaction"; // Custom hook to manage the opening of the New Transaction sheet
 import { useCreateTransaction } from "@/features/transactions/api/use-create-transaction";  // Custom hook to handle transaction creation
+import { useCreateCategory } from "@/features/categories/api/use-create-category";
+import { useGetCategories } from "@/features/categories/api/use-get-categories";
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+import { useCreateAccount } from "@/features/accounts/api/use-create-account";
 
 // Defining the form schema, picking only the 'name' field for validation
 const formSchema = insertTransactionSchema.omit({
@@ -19,6 +23,25 @@ export const NewTransactionSheet = () => {
     // Mutation hook for creating a new transaction
     const mutation = useCreateTransaction();
     // Form submission handler to create a new transaction
+    const categoryQuery = useGetCategories();
+    const categoryMutation = useCreateCategory();
+    const onCreateCategory = (name: string) => categoryMutation.mutate({ 
+        name 
+    });
+    const categoryOptions = (categoryQuery.data ?? []).map((category) => ({ 
+        label: category.name, 
+        value: category.id 
+    }));
+
+    const accountQuery = useGetAccounts();
+    const accountMutation = useCreateAccount();
+    const onCreateAccount = (name: string) => accountMutation.mutate({ 
+        name 
+    });
+    const accountOptions = (accountQuery.data ?? []).map((account) => ({ 
+        label: account.name, 
+        value: account.id 
+    }));
     const onSubmit = (values: FormValues) => {
         mutation.mutate(values, {
             onSuccess: () => {
