11479ae0c71b0307d530c56c3517bcb8b8de9d57
TodorHristov06 <todorhristov2006@gmail.com>
2024-12-06 22:09:03 +0200

    Update added new const and method for changing value

--- components/amount-input.tsx
+++ components/amount-input.tsx
@@ -3,6 +3,7 @@ import { Info, MinusCircle, PlusCircle } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 import { Tooltip, TooltipContent, TooltipTrigger , TooltipProvider} from "@/components/ui/tooltip";
+import { parse } from "path";
 
 type Props = {
     value: string;
@@ -17,6 +18,16 @@ export const AmountInput = ({
     placeholder, 
     disabled 
 }: Props) => { 
+    const parseValue = parseFloat(value);
+    const inIncome = parseValue > 0;
+    const inExpense = parseValue < 0;
+
+    const onReverseValue = () => {
+        if (!value) return;
+        
+        const newValue = parseFloat(value) * -1;
+        onChange(newValue.toString());
+    }
     return (
         <div>
             Hello
