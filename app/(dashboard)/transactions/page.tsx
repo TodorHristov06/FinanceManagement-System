7049000a5fd308152b6343edbf8348718aff7972
TodorHristov06 <todorhristov2006@gmail.com>
2024-12-08 00:31:37 +0200

    Update transaction page filterWord to be payee

--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -52,7 +52,7 @@ const TransactionsPage = () => {
                 </CardHeader>
                 <CardContent>
                     <DataTable 
-                        filterKey="email" 
+                        filterKey="payee" 
                         columns={columns} 
                         data={transactions} 
                         onDelete={(row) => {
