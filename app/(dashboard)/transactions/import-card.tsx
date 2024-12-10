3b79811cd0b4331ae92832e72bc77c1e5f0e0672
TodorHristov06 <todorhristov2006@gmail.com>
2024-12-10 18:54:16 +0200

    Add props to import-card.tsx

--- app/(dashboard)/transactions/import-card.tsx
+++ app/(dashboard)/transactions/import-card.tsx
@@ -1,4 +1,13 @@
-export const ImportCard = () => {
+type Props = {
+    data: string[][];
+    onCancel: () => void
+    onSubmit: (data: any) => void
+}
+export const ImportCard = ({
+    data,
+    onCancel,
+    onSubmit, 
+}: Props) => {
     return(
         <div>
             Import Card
