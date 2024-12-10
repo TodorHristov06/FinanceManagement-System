a197321ac33aa13ad248af9a7d80d18391af11ac
TodorHristov06 <todorhristov2006@gmail.com>
2024-12-10 22:02:43 +0200

    Update import-card map the data for the header and continue option

--- app/(dashboard)/transactions/import-card.tsx
+++ app/(dashboard)/transactions/import-card.tsx
@@ -56,6 +56,31 @@ export const ImportCard = ({
 
     const progress = Object.values(selectedColumns).filter(Boolean).length;
 
+    const handleContinue = () => {
+        const getColumnIndex = (column: string) => {
+            return column.split("_")[1];
+        }
+
+        const mappedData = {
+            headers: headers.map((_header, index) => {
+                const columnIndex = getColumnIndex(`column_${index}`);
+                return selectedColumns[`column_${columnIndex}`] || null;
+            }),
+            body: body.map((row) => {
+                const transformedRow = row.map((cell, index) => {
+                    const columnIndex = getColumnIndex(`column_${index}`);
+                    return selectedColumns[`column_${columnIndex}`] ? cell : null;
+                })
+
+                return transformedRow.every((item) => item === null) 
+                ? [] 
+                : transformedRow;
+            }).filter((row) => row.length > 0)
+        }
+
+        console.log({mappedData})
+    }
+
     return(
         <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
             <Card className="border-none drop-shadow-sm">
@@ -74,7 +99,7 @@ export const ImportCard = ({
                         <Button
                             size="sm"
                             disabled={progress < requiredOptions.length}
-                            onClick={() => {}}
+                            onClick={handleContinue}
                             className="w-full lg:w-auto"
                         >
                             Continue({progress}/{requiredOptions.length})
