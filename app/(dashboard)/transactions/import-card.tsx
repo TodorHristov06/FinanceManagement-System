7c595602aa205d751a6e50c8851d377f5f2316d2
TodorHristov06 <todorhristov2006@gmail.com>
2024-12-10 19:03:28 +0200

    Update import-card added imports and used card components

--- app/(dashboard)/transactions/import-card.tsx
+++ app/(dashboard)/transactions/import-card.tsx
@@ -1,3 +1,6 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+
 const dateFormat = "yyyy-MM-dd HH:mm:ss";
 const outputFormat = "yyyy-MM-dd";
 
@@ -21,8 +24,22 @@ export const ImportCard = ({
     onSubmit, 
 }: Props) => {
     return(
-        <div>
-            Import Card
+        <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+            <Card className="border-none drop-shadow-sm">
+                <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+                    <CardTitle className="text-xl line-clamp-1">
+                        Import Transactions
+                    </CardTitle>
+                    <div className="flex items-center gap-x-2">
+                        <Button onClick={() => {}} size="sm">
+                            Cancel
+                        </Button>
+                    </div>
+                </CardHeader>
+                <CardContent>
+                    Hello
+                </CardContent>
+            </Card>
         </div>
     )
 }
