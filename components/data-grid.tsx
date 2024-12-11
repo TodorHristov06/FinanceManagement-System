b786ee02ddaebbc084c04a7b224a79d02f1026c9
TodorHristov06 <todorhristov2006@gmail.com>
2024-12-11 23:52:28 +0200

    Add and use data-card.tsx component

--- components/data-grid.tsx
+++ components/data-grid.tsx
@@ -1,8 +1,12 @@
 "use client"
 
+import { useGetSummary } from "@/features/summary/api/use-get-summary"
+import { FaPiggyBank } from "react-icons/fa"
 import { formatDateRange } from "@/lib/utils"
 import { useSearchParams } from "next/navigation"
+import { DataCard } from "@/components/data-card"
 export const DataGrid = () => {
+    const {data} = useGetSummary();
     const params = useSearchParams()
     const to = params.get("to") || undefined
     const from = params.get("from") || undefined
@@ -10,7 +14,14 @@ export const DataGrid = () => {
     const dataRangeLabel = formatDateRange({ to, from })
     return (
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 pb-2 mb-8">
-            {dataRangeLabel}
+            <DataCard
+                title="Remaining"
+                value={data?.remainingAmount}
+                PercentageChange={data?.remainingChange}
+                icon={FaPiggyBank}
+                variant="default"
+                dataRange={dataRangeLabel}
+            />
         </div>
     )
 }
