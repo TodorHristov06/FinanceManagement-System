f2a9f5cb4155e7bcfbbf1cd8f6e4193bf10d2817
TodorHristov06 <todorhristov2006@gmail.com>
2024-12-12 17:46:25 +0200

    Update chart.tsx add select button for the different chart types

--- components/chart.tsx
+++ components/chart.tsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
+import { AreaChart, FileSearch } from "lucide-react";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";   
-import { FileSearch } from "lucide-react";
 import { AreaVariant } from "@/components/area-variant";
 import { BarVariant } from "@/components/bar-variant";
 import { LineVariant } from "@/components/line-variant";
@@ -13,13 +15,36 @@ type Props = {
 };
 
 export const Chart = ({ data = [] }: Props) => {
+    const [chartType, setChartType] = useState("area");
+
+    const onTypeChange = (type: string) => {
+        //ToDo: ADD a paywall
+        setChartType(type);
+    }
     return (
         <Card className="border-none drop-shadow-sm">
             <CardHeader className="flex space-y-2 lg:space-y-0 lg:flex-row lg:items-center justify-between">
                 <CardTitle className="text-xl line-clamp-1">
                     Transactions
                 </CardTitle>
-                {/*TODO: Add select*/}
+                <Select 
+                    defaultValue={chartType}
+                    onValueChange={onTypeChange}
+                >
+                    <SelectTrigger className="lg:w-auto h-9 rounded-md px-3">
+                        <SelectValue placeholder="Chart type" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectItem value="area">
+                            <div className="flex items-center">
+                                <AreaChart className="size-4 mr-2 shrink-0" />
+                                <p className="line-clamp-1">
+                                    Area Chart
+                                </p>
+                            </div>
+                        </SelectItem>
+                    </SelectContent>
+                </Select>
             </CardHeader>
             <CardContent>
                 {data.length === 0 ? (
