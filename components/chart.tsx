18dbb546bebd3aac12b94d6468101876dfd3cc76
TodorHristov06 <todorhristov2006@gmail.com>
2024-12-12 17:49:41 +0200

    Update chart.tsx ad selectItem for barChart

--- components/chart.tsx
+++ components/chart.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { AreaChart, FileSearch, LineChart } from "lucide-react";
+import { AreaChart, BarChart, FileSearch, LineChart } from "lucide-react";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";   
 import { AreaVariant } from "@/components/area-variant";
@@ -48,6 +48,14 @@ export const Chart = ({ data = [] }: Props) => {
                                 <LineChart className="size-4 mr-2 shrink-0" />
                                 <p className="line-clamp-1">
                                     Line Chart
+                                </p>
+                            </div>
+                        </SelectItem>
+                        <SelectItem value="bar">
+                            <div className="flex items-center">
+                                <BarChart className="size-4 mr-2 shrink-0" />
+                                <p className="line-clamp-1">
+                                    Bar Chart
                                 </p>
                             </div>
                         </SelectItem>
