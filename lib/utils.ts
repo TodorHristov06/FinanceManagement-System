e2beea2f3bcdce0584c4cf5266c8840624aa1c80
Todor Hristov <todorhristov2006@gmail.com>
2025-02-02 16:51:20 +0200

    Update lib/ file comments

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,20 +1,24 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 import { eachDayOfInterval, format, isSameDay, subDays } from "date-fns";
+
+// Utility function to merge class names using Tailwind's merge function
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
+// Converts an amount from miliunits to base units (USD)
 export function convertAmountFromMiliunits(amount: number) {
   return amount / 1000;
 }
 
+// Converts an amount from base units (USD) to miliunits
 export function convertAmountToMiliunits(amount: number) {
   return Math.round(amount * 1000);
 }
 
+// Formats a numeric value into currency format (USD)
 export function formatCurrency(value: number) {
-  
   return Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
@@ -22,6 +26,7 @@ export function formatCurrency(value: number) {
   }).format(value);
 }
 
+// Calculates the percentage change between two values
 export function calculatePercentageChange(
   current: number, 
   previous: number,
@@ -29,10 +34,10 @@ export function calculatePercentageChange(
   if (previous === 0) {
     return previous === current ? 0 : 100;
   }
-
   return ((current - previous) / previous) * 100;
 }
 
+// Fills in missing days with zero values for income and expenses within a given date range
 export function fillMissingDays (
   activeDays: {
     date: Date,
@@ -51,6 +56,7 @@ export function fillMissingDays (
     end: endDate,
   });
 
+  // Map over all the days in the range and fill missing data with zeros
   const transactionsByDay = allDays.map((day) => {
     const found = activeDays.find((d) => isSameDay(d.date, day));
 
@@ -68,11 +74,13 @@ export function fillMissingDays (
   return transactionsByDay
 }
 
+// Type definition for a date range period
 type Period = {
   from: string | Date | undefined
   to: string | Date | undefined
 }
 
+// Formats a date range for display, e.g., 'Jan 01 - Feb 01, 2025'
 export function formatDateRange (period?: Period) {
   const defaultTo = new Date();
   const defaultFrom = subDays(defaultTo, 30);
@@ -88,6 +96,7 @@ export function formatDateRange (period?: Period) {
   return format(period.from, "LLL dd, y");
 }
 
+// Formats a percentage value with an optional prefix
 export function formatPercentage (
   value: number,
   options: { addPrefix?: boolean} = {
@@ -98,6 +107,7 @@ export function formatPercentage (
     style: "percent",
   }).format(value / 100);
 
+  // Optionally adds a plus sign if the value is positive and addPrefix is true
   if (options.addPrefix && value > 0) {
     return `+${result}`;  
   }
